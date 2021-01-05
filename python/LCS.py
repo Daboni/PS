f4836ca8b323a15c2fded18713518460774f41c0
Daboni <60393977+Daboni@users.noreply.github.com>
2021-01-05 14:36:32 +0900

    Add files via upload

---
+++ python/LCS.py
@@ -0,0 +1,18 @@
+a = "AGCAT"
+b = "GAC"
+
+c = [ [ 0 for col in range(len(b)+1)] for row in range(len(a)+1)]
+
+def lcs(x,y):
+    for i in range(1,len(a)+1):
+        for j in range(1,len(b)+1):
+            
+            if a[i-1] == b[j-1] :
+                c[i][j] = c[i-1][j-1]+1
+            else :
+                c[i][j] = max(c[i][j-1],c[i-1][j])
+
+    return c[i][j]
+
+
+print(lcs(a,b))
