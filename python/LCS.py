be040967e13f2ca474c14ec798ae692478a87e20
Daboni <60393977+Daboni@users.noreply.github.com>
2021-01-05 14:39:00 +0900

    Update LCS.py

--- python/LCS.py
+++ python/LCS.py
@@ -1,5 +1,15 @@
-a = "AGCAT"
-b = "GAC"
+#백준 9251번 LCS
+"""
+문제
+
+LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열이 주어졌을 때, 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
+
+예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
+
+"""
+
+a = input()
+b = input()
 
 c = [ [ 0 for col in range(len(b)+1)] for row in range(len(a)+1)]
 
