344d8f74e64dcd582f80ae09f36ea9002e1ab1ca
Daboni <60393977+Daboni@users.noreply.github.com>
2021-01-14 18:45:42 +0900

    Add files via upload

---
+++ python/동전0.py
@@ -0,0 +1,19 @@
+a = list(map(int,input().split()))
+
+coins = []
+
+for i in range(a[0]):
+    coins.append(int(input()))
+
+coins.reverse()
+
+price = a[1]
+
+res = 0
+
+for coin in coins:
+
+    if price == 0:
+        break
+    res += price // coin
+    price %= coin
